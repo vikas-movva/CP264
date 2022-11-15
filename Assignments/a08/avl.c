c753d3b435655c79bbb0baa81c29f2d99f8afb07
Vikas Movva <vikas.s.movva@gmail.com>
2022-11-14 19:00:21 -0500

    fix if statements

--- Assignments/a08/avl.c
+++ Assignments/a08/avl.c
@@ -1,36 +1,38 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "avl.h"
 
 // A utility functions
 int max(int a, int b)
 {
-    return (a > b)? a : b;
+    return (a > b) ? a : b;
 }
 
 int height(TNODE *np)
 {
-// your implementation
-    if(np == NULL)
+    // your implementation
+    if (np == NULL)
         return 0;
     else
         return np->height;
 }
 
-int balance_factor(TNODE* np) {
-// your implementation
-    if(np == NULL)
+int balance_factor(TNODE *np)
+{
+    // your implementation
+    if (np == NULL)
         return 0;
     else
         return height(np->left) - height(np->right);
 }
 
-int is_avl(TNODE *root) {
-// your implementation
-    if(root == NULL)
+int is_avl(TNODE *root)
+{
+    // your implementation
+    if (root == NULL)
         return 1;
-    else if(abs(balance_factor(root)) > 1)
+    else if (abs(balance_factor(root)) > 1)
         return 0;
     else
         return is_avl(root->left) && is_avl(root->right);
@@ -38,7 +40,7 @@ int is_avl(TNODE *root) {
 
 TNODE *rotate_right(TNODE *y)
 {
-// your implementation
+    // your implementation
     TNODE *x = y->left;
     TNODE *T2 = x->right;
     x->right = y;
@@ -50,7 +52,7 @@ TNODE *rotate_right(TNODE *y)
 
 TNODE *rotate_left(TNODE *x)
 {
-// your implementation
+    // your implementation
     TNODE *y = x->right;
     TNODE *T2 = y->left;
     y->left = x;
@@ -62,185 +64,226 @@ TNODE *rotate_left(TNODE *x)
 
 void insert(TNODE **rootp, char *name, float score)
 {
-  TNODE *np = (TNODE *) malloc(sizeof(TNODE));
-  if (np == NULL) return;
-  strcpy(np->data.name, name);
-  np->data.score = score;
-  np->height = 1;
-  np->left = NULL;
-  np->right = NULL;
-  
-  // 1. Perform the normal BST insertion
-  if (*rootp == NULL) {
-    *rootp = np;
-    return;
-  }
-  
-  TNODE *root = *rootp;
-  if (strcmp(name, root->data.name) < 0 )
-    insert(&root->left, name, score);
-  else if (strcmp(name, root->data.name) > 0 )
-    insert(&root->right, name, score);
-  else return ;
+    TNODE *np = (TNODE *)malloc(sizeof(TNODE));
+    if (np == NULL)
+        return;
+    strcpy(np->data.name, name);
+    np->data.score = score;
+    np->height = 1;
+    np->left = NULL;
+    np->right = NULL;
 
-  // 2. update height of this root node
+    // 1. Perform the normal BST insertion
+    if (*rootp == NULL)
+    {
+        *rootp = np;
+        return;
+    }
+
+    TNODE *root = *rootp;
+    if (strcmp(name, root->data.name) < 0)
+        insert(&root->left, name, score);
+    else if (strcmp(name, root->data.name) > 0)
+        insert(&root->right, name, score);
+    else
+        return;
+
+    // 2. update height of this root node
     root->height = 1 + max(height(root->left), height(root->right));
-  // 3. Get the balance factor of this ancestor node to check whether this node became unbalanced
+    // 3. Get the balance factor of this ancestor node to check whether this node became unbalanced
     int balance = balance_factor(root);
-  // 4. re-balance if not balanced
+    // 4. re-balance if not balanced
     // 4.1 Left Left Case
     if (balance > 1 && strcmp(name, root->left->data.name) < 0)
         *rootp = rotate_right(root);
     // 4.2 Right Right Case
-    if (balance < -1 && strcmp(name, root->right->data.name) > 0)
+    else if (balance < -1 && strcmp(name, root->right->data.name) > 0)
         *rootp = rotate_left(root);
     // 4.3 Left Right Case
-    if (balance > 1 && strcmp(name, root->left->data.name) > 0) {
+    else if (balance > 1 && strcmp(name, root->left->data.name) > 0)
+    {
         root->left = rotate_left(root->left);
         *rootp = rotate_right(root);
     }
     // 4.4 Right Left Case
-    if (balance < -1 && strcmp(name, root->right->data.name) < 0) {
+    else if (balance < -1 && strcmp(name, root->right->data.name) < 0)
+    {
         root->right = rotate_right(root->right);
         *rootp = rotate_left(root);
     }
-
 }
 
-void delete(TNODE **rootp, char *name)
+void delete (TNODE **rootp, char *name)
 {
-  TNODE *root = *rootp;
-  TNODE* np;
+    TNODE *root = *rootp;
+    TNODE *np;
 
-  if (root == NULL) return;
+    if (root == NULL)
+        return;
 
-  if (strcmp(name, root->data.name) == 0) {
-    if (root->left == NULL && root->right == NULL) {
-      free(root);
-      *rootp = NULL;
-    } else if (root->left != NULL && root->right == NULL) {
-      np = root->left;
-      free(root);
-      *rootp = np;
-    } else if (root->left == NULL && root->right != NULL) {
-      np = root->right;
-      free(root);
-      *rootp = np;
-    } else if (root->left != NULL && root->right != NULL) {
-      np = extract_smallest_node(&root->right);
-      np->left = root->left;
-      np->right = root->right;
-      free(root);
-      *rootp = np;
+    if (strcmp(name, root->data.name) == 0)
+    {
+        if (root->left == NULL && root->right == NULL)
+        {
+            free(root);
+            *rootp = NULL;
+        }
+        else if (root->left != NULL && root->right == NULL)
+        {
+            np = root->left;
+            free(root);
+            *rootp = np;
+        }
+        else if (root->left == NULL && root->right != NULL)
+        {
+            np = root->right;
+            free(root);
+            *rootp = np;
+        }
+        else if (root->left != NULL && root->right != NULL)
+        {
+            np = extract_smallest_node(&root->right);
+            np->left = root->left;
+            np->right = root->right;
+            free(root);
+            *rootp = np;
+        }
     }
-  } else {
-    if (strcmp(name, root->data.name) < 0) {
-      delete(&root->left, name);
-    } else {
-      delete(&root->right, name);
+    else
+    {
+        if (strcmp(name, root->data.name) < 0)
+        {
+            delete (&root->left, name);
+        }
+        else
+        {
+            delete (&root->right, name);
+        }
     }
-  }
 
     // If the tree had only one node then return
-  if (*rootp == NULL) return;
+    if (*rootp == NULL)
+        return;
 
-  root = *rootp;
-  
-  // STEP 2: update the this root node's height
+    root = *rootp;
+
+    // STEP 2: update the this root node's height
     root->height = 1 + max(height(root->left), height(root->right));
 
-  // STEP 3: get the balance factor of this root node
+    // STEP 3: get the balance factor of this root node
     int balance = balance_factor(root);
-  // STEP 4: re-balance if not balanced
+    // STEP 4: re-balance if not balanced
     // 4.1 Left Left Case
     if (balance > 1 && balance_factor(root->left) >= 0)
         *rootp = rotate_right(root);
     // 4.2 Right Right Case
-    if (balance < -1 && balance_factor(root->right) <= 0)
+    else if (balance < -1 && balance_factor(root->right) <= 0)
         *rootp = rotate_left(root);
     // 4.3 Left Right Case
-    if (balance > 1 && balance_factor(root->left) < 0) {
+    else if (balance > 1 && balance_factor(root->left) < 0)
+    {
         root->left = rotate_left(root->left);
         *rootp = rotate_right(root);
     }
     // 4.4 Right Left Case
-    if (balance < -1 && balance_factor(root->right) > 0) {
+    else if (balance < -1 && balance_factor(root->right) > 0)
+    {
         root->right = rotate_right(root->right);
         *rootp = rotate_left(root);
     }
 }
 
-
 // following functions are from bst.c of a7
-TNODE *extract_smallest_node(TNODE **rootp) {
-  TNODE *tnp = *rootp;
-  TNODE *parent = NULL;
-  if (tnp == NULL) {
-    return NULL;
-  } else {
-    while (tnp->left) {
-      parent = tnp;
-      tnp = tnp->left;
+TNODE *extract_smallest_node(TNODE **rootp)
+{
+    TNODE *tnp = *rootp;
+    TNODE *parent = NULL;
+    if (tnp == NULL)
+    {
+        return NULL;
     }
-    if (parent == NULL)
-      *rootp = tnp->right;
     else
-      parent->left = tnp->right;
-    tnp->left = NULL;
-    tnp->right = NULL;
-    return tnp;
-  }
+    {
+        while (tnp->left)
+        {
+            parent = tnp;
+            tnp = tnp->left;
+        }
+        if (parent == NULL)
+            *rootp = tnp->right;
+        else
+            parent->left = tnp->right;
+        tnp->left = NULL;
+        tnp->right = NULL;
+        return tnp;
+    }
 }
 
-TNODE *search(TNODE *root, char *name) {
-  TNODE *tp = root;
-  while (tp) {
-    if (strcmp(name, tp->data.name) == 0) {
-       return tp;
-    } else if (strcmp(name, tp->data.name) < 0) 
-      tp = tp->left;
-     else 
-      tp = tp->right;
-  }    
-  return NULL;
+TNODE *search(TNODE *root, char *name)
+{
+    TNODE *tp = root;
+    while (tp)
+    {
+        if (strcmp(name, tp->data.name) == 0)
+        {
+            return tp;
+        }
+        else if (strcmp(name, tp->data.name) < 0)
+            tp = tp->left;
+        else
+            tp = tp->right;
+    }
+    return NULL;
 }
 
-void clean_tree(TNODE **rootp) {
-  if (*rootp) {
-    TNODE *np = *rootp;
-    if (np->left)
-      clean_tree(&np->left);
-    if (np->right)
-      clean_tree(&np->right);
-    free(np);
-  }
-  *rootp = NULL;
+void clean_tree(TNODE **rootp)
+{
+    if (*rootp)
+    {
+        TNODE *np = *rootp;
+        if (np->left)
+            clean_tree(&np->left);
+        if (np->right)
+            clean_tree(&np->right);
+        free(np);
+    }
+    *rootp = NULL;
 }
 
-void display_inorder(TNODE *root) {
-  if (root) {
-    if (root->left) display_inorder(root->left);
-    printf("(%s %3.1f) ", root->data.name, root->data.score);
-    if (root->right) display_inorder(root->right);
-  }
+void display_inorder(TNODE *root)
+{
+    if (root)
+    {
+        if (root->left)
+            display_inorder(root->left);
+        printf("(%s %3.1f) ", root->data.name, root->data.score);
+        if (root->right)
+            display_inorder(root->right);
+    }
 }
 
-void display_inorder_lines(TNODE *root) {
-  if (root) {
-    if (root->left) display_inorder_lines(root->left);
-    printf("%-15s%.1f\n", root->data.name, root->data.score);
-    if (root->right) display_inorder_lines(root->right);
-  }
+void display_inorder_lines(TNODE *root)
+{
+    if (root)
+    {
+        if (root->left)
+            display_inorder_lines(root->left);
+        printf("%-15s%.1f\n", root->data.name, root->data.score);
+        if (root->right)
+            display_inorder_lines(root->right);
+    }
 }
 
-void display_tree(TNODE *root, int prelen) {
-  if (root) {
-    int i;
-    for (i = 0; i < prelen; i++)
-      printf("%c", ' ');
-    printf("%s", "|___");
-    printf("%s %.1f %d\n", root->data.name, root->data.score, root->height);
-    display_tree(root->right, prelen + 4);    
-    display_tree(root->left, prelen + 4);
-  }
+void display_tree(TNODE *root, int prelen)
+{
+    if (root)
+    {
+        int i;
+        for (i = 0; i < prelen; i++)
+            printf("%c", ' ');
+        printf("%s", "|___");
+        printf("%s %.1f %d\n", root->data.name, root->data.score, root->height);
+        display_tree(root->right, prelen + 4);
+        display_tree(root->left, prelen + 4);
+    }
 }
